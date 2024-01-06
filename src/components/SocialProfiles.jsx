ad02cedc107af6afa66bb8e9b43479283a05cd52
rishabhhhhhhhh <p.rishabh.arora@gmail.com>
2024-01-06 19:25:53 +0530

    adding profile links

--- src/components/SocialProfiles.jsx
+++ src/components/SocialProfiles.jsx
@@ -1,78 +1,68 @@
-import React from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 //TODO : Update data about social links and profiles
 
 const SocialProfiles = () => {
-
   const profileDetails = [
     {
-        id: 1,
-        child: (
-            <>
-                LinkedIn
-                <FaLinkedin size={30}/>
-            </>
-        ),
-        href: "https://www.google.com",
-        style: "hover:rounded-tr-md"
+      id: 1,
+      child: (
+        <>
+          LinkedIn
+          <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/rishabhvarora/",
+      style: "hover:rounded-tr-md",
     },
     {
-        id: 2,
-        child: (
-            <>
-                Github
-                <FaGithub size={30}/>
-            </>
-        ),
-        href: "https://www.google.com",
-        style: ""
+      id: 2,
+      child: (
+        <>
+          Github
+          <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/rishabhhhhhhhh",
+      style: "",
     },
     {
-        id: 3,
-        child: (
-            <>
-                Mail
-                <HiOutlineMail size={30}/>
-            </>
-        ),
-        href: "https://www.google.com",
-        style: ""
+      id: 3,
+      child: (
+        <>
+          Mail
+          <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:rishabh.arora266@gmail.com",
+      style: "",
     },
-    {
-        id: 4,
-        child: (
-            <>
-                Contact
-                <BsFillPersonLinesFill size={30}/>
-            </>
-        ),
-        href: "https://www.google.com",
-        style: "hover:rounded-br-md"
-    }
   ];
 
   return (
-    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
-        <ul>
-            {
-                profileDetails.map(({child, id, href, style}) => {
-                    return (
-                        <li key={id} className={`flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] ${style} duration-300`}>
-                            <a className='w-full text-white flex justify-between items-center' href={href}>
-                                {
-                                    child
-                                }     
-                            </a>
-                        </li>
-                    )
-                })
-            }
-        </ul>
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {profileDetails.map(({ child, id, href, style }) => {
+          return (
+            <li
+              key={id}
+              className={`flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] ${style} duration-300`}
+            >
+              <a
+                className="w-full text-white flex justify-between items-center"
+                href={href}
+              >
+                {child}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SocialProfiles
+export default SocialProfiles;
