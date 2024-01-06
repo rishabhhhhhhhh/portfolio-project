b7c30e1049ea2addf6a36110ace6b42e85183bb6
rishabhhhhhhhh <p.rishabh.arora@gmail.com>
2024-01-06 19:28:16 +0530

    removing warnings

--- src/components/SocialProfiles.jsx
+++ src/components/SocialProfiles.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-import { BsFillPersonLinesFill } from "react-icons/bs";
-
-//TODO : Update data about social links and profiles
 
 const SocialProfiles = () => {
   const profileDetails = [
